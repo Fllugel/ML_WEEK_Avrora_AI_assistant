d4b630fc13007cb777acbf66df4173d042a7b962
Fllugel <135070466+Fllugel@users.noreply.github.com>
2025-01-29 21:31:01 +0200

    update avrora_info_tool prompt

--- Tools/holiday_info_tool.py
+++ Tools/holiday_info_tool.py
@@ -1,8 +1,8 @@
 from langchain_core.tools import tool
 
 
-@tool("avrora_info_tool")
-def avrora_info_tool(question: str) -> str:
+@tool("holiday_info_tool")
+def holiday_info_tool(question: str) -> str:
     """
     Цей інструмент повертає інформацію про компанію "Аврора".
     """
