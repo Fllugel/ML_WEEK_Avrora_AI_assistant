e60fd66b8a25afece71b1639163c6936f14f81e6
MarinaChorna <113356204+MarinaChrn@users.noreply.github.com>
2025-01-31 10:26:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://localhost:3000", "marinachrn.github.io/avrora/"],
+    allow_origins=["http://localhost:3000", "https://localhost:3000", "https://marinachrn.github.io/avrora/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
