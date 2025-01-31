2d5aa02d23cecc85d805b363f5fba50e3ae3cb70
Fllugel <135070466+Fllugel@users.noreply.github.com>
2025-01-31 04:11:43 +0200

    allow HTTPS requests

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow frontend origin
+    allow_origins=["http://localhost:3000", "https://localhost:3000"],  # Allow frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
